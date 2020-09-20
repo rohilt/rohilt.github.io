728f5e40c7b52bf82b14a8fd1cf9cf829ad0c65f
Rohil Tuli <rohil.tuli@gmail.com>
2020-09-20 14:29:43 -0400

    Updates

--- scripts/gh-pages.js
+++ scripts/gh-pages.js
@@ -2,15 +2,16 @@ var ghpages = require('gh-pages');
 
 ghpages.publish(
     '__sapper__/export',// <-- replace yourproject with your repo name
-    {
-        branch: 'master',
-        repo: 'rohilt.github.io',
-        user: {
-            name: 'Rohil Tuli',
-            email: 'rohil.tuli@gmail.com'
-        }
-    },
-    () => {
+    // {
+    //     branch: 'master',
+    //     repo: 'https://www.github.com/rohilt/rohilt.github.io',
+    //     user: {
+    //         name: 'rohilt',
+    //         email: 'rohil.tuli@gmail.com'
+    //     }
+    // },
+    (err) => {
+        console.log(err)
         console.log('Deploy Complete!')
     }
 )
